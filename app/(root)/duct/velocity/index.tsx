fa149922490b718d7009ea2310c88f00f7610e3e
derRaab <github.com@derRaab.com>
2024-06-04 13:09:35 +0200

    Rough duct velocity calculator

--- app/(root)/duct/velocity/index.tsx
+++ app/(root)/duct/velocity/index.tsx
@@ -1,16 +1,63 @@
-import { translate } from "@/src/localization";
-import { Text, View } from "react-native";
+import { Calculation, calculateDuctVelocity } from "@/src/calculation";
+import { Calculator } from "@/src/components/Calculator/Calculator";
+import { usePreferredLayout } from "@/src/themes/hooks";
+import { useHeaderHeight } from "@react-navigation/elements";
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Index() {
+  // NOTE: Header height contains safe area inset top!
+  const headerHeight = useHeaderHeight();
+  const layout = usePreferredLayout();
+  const safeAreaInsets = useSafeAreaInsets();
+
+  // Fullscreen calculator will also deal with safe area insets
+  const calculatorInsets = {
+    bottom: safeAreaInsets.bottom,
+    left: Math.max(safeAreaInsets.left, layout.padding),
+    right: Math.max(safeAreaInsets.right, layout.padding),
+    top: headerHeight,
+  };
+
+  // Calculation state
+  const [calculation, setCalculation] = useState<Calculation>({
+    area: 0,
+    areaUnit: "m2",
+    diameter: 0,
+    diameterUnit: "mm",
+    flowrate: 0,
+    flowrateUnit: "m3_h",
+    height: 0,
+    heightUnit: "mm",
+    object: "duct",
+    result: 0,
+    resultUnit: "m_s",
+    type: "velocity",
+    width: 0,
+    widthUnit: "mm",
+  });
+
+  // Handle calculator input change
+  const onCalculatorInputChange = (calculation: Calculation) => {
+    const newCalculation = calculateDuctVelocity(calculation);
+    setCalculation(newCalculation);
+  };
+
   return (
-    <View
-      style={{
-        alignItems: "center",
-        flex: 1,
-        justifyContent: "center",
-      }}
-    >
-      <Text>{translate("duct") + " > " + translate("velocity")}</Text>
+    <View style={styles.container}>
+      <Calculator
+        layout={layout}
+        calculation={calculation}
+        insets={calculatorInsets}
+        onInputChange={onCalculatorInputChange}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+  },
+});
