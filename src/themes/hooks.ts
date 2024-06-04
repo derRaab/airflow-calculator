6ef00ee6178cbafda45a54ac9cddc32d14ab2654
derRaab <github.com@derRaab.com>
2024-06-04 12:24:57 +0200

    Update hooks.ts

--- src/themes/hooks.ts
+++ src/themes/hooks.ts
@@ -1,8 +1,19 @@
-import { useColorScheme } from "react-native";
+import { useColorScheme, useWindowDimensions } from "react-native";
 import { materialTheme } from "./colors";
+import { MaterialDesign3Layout, layout } from "./layout";
 import { selectColorScheme } from "./m3/MaterialDesign3ColorTheme";
+import {
+  getWindowSizeClassName,
+  selectWindowSizeClass,
+} from "./m3/MaterialDesign3Layout";
 
 export const usePreferredColorScheme = () => {
   let colorSchemeName = useColorScheme();
   return selectColorScheme(materialTheme.schemes, colorSchemeName);
+};
+
+export const usePreferredLayout = () => {
+  const { width } = useWindowDimensions();
+  const className = getWindowSizeClassName(width);
+  return selectWindowSizeClass(layout, className) as MaterialDesign3Layout;
 };
