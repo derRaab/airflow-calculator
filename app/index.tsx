b586a1b36dabd84637fc8600b5faf6b91a0f9b16
derRaab <github.com@derRaab.com>
2024-06-17 20:58:30 +0200

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,8 @@
 import { CalculationSelector } from "@/src/components/CalculationSelector/CalculationSelector";
-import { usePreferredLayout } from "@/src/themes/hooks";
+import {
+  usePreferredColorScheme,
+  usePreferredLayout,
+} from "@/src/themes/hooks";
 import { Link } from "expo-router";
 import { View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
@@ -7,47 +10,62 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 export default function Index() {
   // NOTE: Header height contains safe area inset top!
   const layout = usePreferredLayout();
+  const colorScheme = usePreferredColorScheme();
   const safeAreaInsets = useSafeAreaInsets();
 
   // Fullscreen calculator will also deal with safe area insets
   const selectorInsets = {
-    bottom: layout.padding,
+    bottom: 0,
     left: Math.max(safeAreaInsets.left, layout.padding),
     right: Math.max(safeAreaInsets.right, layout.padding),
-    top: Math.max(safeAreaInsets.top, layout.padding),
+    top: 0,
   };
 
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1 }}>
-        <View
-          style={{
-            flex: 1,
-            flexDirection: "column",
-          }}
-        >
-          <CalculationSelector
-            insets={selectorInsets}
-            layout={layout}
-            object="duct"
-          />
-          <CalculationSelector
-            insets={selectorInsets}
-            layout={layout}
-            object="pipe"
-          />
-        </View>
-        <View style={{ height: 44 }}>
-          <View
-            style={{
-              flex: 1,
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <Link href="./(root)/info">Info</Link>
-          </View>
-        </View>
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "column",
+        backgroundColor: colorScheme.surface,
+        gap: layout.gap,
+        paddingLeft: layout.padding,
+        paddingRight: layout.padding,
+        paddingTop: safeAreaInsets.top,
+        paddingBottom: safeAreaInsets.bottom,
+      }}
+    >
+      <View
+        style={{
+          flexShrink: 1,
+        }}
+      >
+        <CalculationSelector
+          insets={selectorInsets}
+          layout={layout}
+          object="duct"
+        />
+      </View>
+      <View
+        style={{
+          flexShrink: 1,
+        }}
+      >
+        <CalculationSelector
+          insets={selectorInsets}
+          layout={layout}
+          object="pipe"
+        />
+      </View>
+      <View
+        style={{
+          height: 44,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Link href="./(root)/info" style={{ color: colorScheme.onSurface }}>
+          Info
+        </Link>
       </View>
     </View>
   );
