87ec317bfca46a4834974c31e581a9e0f3a9d8b4
derRaab <github.com@derRaab.com>
2024-06-17 20:58:34 +0200

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,38 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
+import { THREE } from "expo-three";
+import { useColorScheme } from "react-native";
+
+// This is needed to make THREE global
+global.THREE = global.THREE || THREE;
 
 export default function RootLayout() {
+  let colorSchemeName = useColorScheme();
+
   return (
-    <Stack
-      screenOptions={{
-        // Only the back button on all screens
-        headerBackTitleVisible: false,
-        headerShown: true,
-        headerTitle: "",
-        headerTransparent: true,
-      }}
+    <ThemeProvider
+      value={colorSchemeName === "dark" ? DarkTheme : DefaultTheme}
     >
-      <Stack.Screen
-        name="index"
-        options={{
-          headerShown: false,
+      <Stack
+        screenOptions={{
+          // Only the back button on all screens
+          headerBackTitleVisible: false,
+          headerShown: true,
+          headerTitle: "",
+          headerTransparent: true,
         }}
-      />
-    </Stack>
+      >
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </ThemeProvider>
   );
 }
