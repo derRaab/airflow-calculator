9bfedd1301b0c7d68df3ae0628caedc249951e41
derRaab <github.com@derRaab.com>
2024-07-04 20:25:22 +0200

    Load markdown assets

--- app/(root)/info/index.tsx
+++ app/(root)/info/index.tsx
@@ -1,8 +1,12 @@
-import { ThreeObject } from "@/src/components/Three/ThreeObject";
+import { translate } from "@/src/localization";
 import { usePreferredColorScheme } from "@/src/themes/hooks";
+import { typography } from "@/src/themes/typography";
 import { useHeaderHeight } from "@react-navigation/elements";
-import React from "react";
-import { Text, View } from "react-native";
+import { useAssets } from "expo-asset";
+import * as FileSystem from "expo-file-system";
+import { useNavigation } from "expo-router";
+import React, { useEffect, useState } from "react";
+import { Text, TextStyle, View, ViewStyle } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Index() {
@@ -10,31 +14,52 @@ export default function Index() {
   // NOTE: Header height contains safe area inset top!
   const headerHeight = useHeaderHeight();
   const safeAreaInsets = useSafeAreaInsets();
-  console.log("safeAreaInsets", safeAreaInsets);
 
-  const objectWidth = "100%";
+  useNavigation().setOptions({
+    headerTintColor: colorScheme.onBackground,
+  });
+
+  const [markdownContent, setMarkdownContent] = useState<string>("");
+  const [assets] = useAssets([
+    require("../../../assets/markdown/info/de.md"),
+    require("../../../assets/markdown/info/en.md"),
+  ]);
+
+  useEffect(() => {
+    (async () => {
+      if (!assets || assets.length === 0) {
+        return;
+      }
+      const asset = translate("iso") === "de" ? assets[0] : assets[1];
+      await asset.downloadAsync();
+
+      const localUri = asset.localUri ?? "";
+      const content = await FileSystem.readAsStringAsync(localUri);
+      setMarkdownContent(content);
+    })();
+  }, [assets]);
+
+  const rootContainerStyle: ViewStyle = {
+    alignItems: "center",
+    paddingTop: headerHeight,
+    paddingLeft: safeAreaInsets.left,
+    paddingRight: safeAreaInsets.right,
+    paddingBottom: safeAreaInsets.bottom,
+    flex: 1,
+    gap: 10,
+    justifyContent: "center",
+    backgroundColor: colorScheme.background,
+  };
+
+  const textStyle: TextStyle = {
+    ...typography.bodyLarge,
+
+    color: colorScheme.onBackground,
+  };
 
   return (
-    <View
-      style={{
-        alignItems: "center",
-        paddingTop: headerHeight,
-        paddingLeft: safeAreaInsets.left,
-        paddingRight: safeAreaInsets.right,
-        paddingBottom: safeAreaInsets.bottom,
-        flex: 1,
-        gap: 10,
-        justifyContent: "center",
-      }}
-    >
-      <Text>Info</Text>
-      <View
-        style={{
-          width: objectWidth,
-        }}
-      >
-        <ThreeObject colorScheme={colorScheme} object="both" />
-      </View>
+    <View style={rootContainerStyle}>
+      <Text style={textStyle}>{markdownContent}</Text>
     </View>
   );
 }
