9ad84f703573e02d50ee14eaf2ec135204b247bf
derRaab <github.com@derRaab.com>
2024-07-05 16:31:36 +0200

    Update CalculatorInputAccessoryView.tsx

--- src/components/Calculator/CalculatorInputAccessoryView.tsx
+++ src/components/Calculator/CalculatorInputAccessoryView.tsx
@@ -1,15 +1,9 @@
 import { MaterialDesign3Layout } from "@/src/themes/layout";
 import { MaterialDesign3ColorScheme } from "@/src/themes/m3/MaterialDesign3ColorTheme";
 import { typography } from "@/src/themes/typography";
+import { createCachedFactory } from "@/src/utils/factoryUtils";
 import React, { FC } from "react";
-import {
-  Pressable,
-  Text,
-  TextInput,
-  TextStyle,
-  View,
-  ViewStyle,
-} from "react-native";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 
 export interface CalculatorInputAccessorySetup {
   currentLabel?: string;
@@ -35,59 +29,38 @@ interface CalculatorInputAccessoryViewProps {
 export const CalculatorInputAccessoryView: FC<
   CalculatorInputAccessoryViewProps
 > = ({ colorScheme, layout, setup }) => {
-  const containerStyle: ViewStyle = {
-    backgroundColor: colorScheme.surfaceContainerHighest,
-    width: "100%",
-  };
-
-  const buttonContainerStyle: ViewStyle = {
-    alignItems: "center",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    padding: layout.padding,
-    width: "100%",
-  };
-
-  const buttonStyle: ViewStyle = {
-    backgroundColor: colorScheme.primary,
-    borderRadius: 1000,
-    padding: layout.padding / 2,
-    paddingHorizontal: layout.padding,
-  };
-
-  const buttonTextStyle: TextStyle = {
-    ...typography.labelLarge,
-    color: colorScheme.onPrimary,
-  };
-
-  const currentTextStyle: TextStyle = {
-    ...typography.labelLarge,
-    color: colorScheme.onSurface,
-  };
+  const styles = useLocalStyle(colorScheme, layout);
 
   if (!setup.keyboardShown) {
     return null;
   }
 
   return (
-    <View style={containerStyle}>
-      <View style={buttonContainerStyle}>
+    <View style={styles.containerStyle}>
+      <View style={styles.buttonContainerStyle}>
         <View>
           {setup.previousLabel && (
-            <Pressable onPress={setup.onPreviousPress} style={buttonStyle}>
-              <Text style={buttonTextStyle}>{"<  " + setup.previousLabel}</Text>
+            <Pressable
+              onPress={setup.onPreviousPress}
+              style={styles.buttonStyle}
+            >
+              <Text style={styles.buttonTextStyle}>
+                {"<  " + setup.previousLabel}
+              </Text>
             </Pressable>
           )}
         </View>
         <View>
           {setup.currentLabel && (
-            <Text style={currentTextStyle}>{setup.currentLabel}</Text>
+            <Text style={styles.currentTextStyle}>{setup.currentLabel}</Text>
           )}
         </View>
         <View>
           {setup.nextLabel && (
-            <Pressable onPress={setup.onNextPress} style={buttonStyle}>
-              <Text style={buttonTextStyle}>{setup.nextLabel + "  >"}</Text>
+            <Pressable onPress={setup.onNextPress} style={styles.buttonStyle}>
+              <Text style={styles.buttonTextStyle}>
+                {setup.nextLabel + "  >"}
+              </Text>
             </Pressable>
           )}
         </View>
@@ -95,3 +68,43 @@ export const CalculatorInputAccessoryView: FC<
     </View>
   );
 };
+
+const createStyleSheet = (
+  colorScheme: MaterialDesign3ColorScheme,
+  layout: MaterialDesign3Layout,
+) => {
+  return StyleSheet.create({
+    containerStyle: {
+      backgroundColor: colorScheme.surfaceContainerHighest,
+      width: "100%",
+    },
+
+    buttonContainerStyle: {
+      alignItems: "center",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      padding: layout.padding,
+      width: "100%",
+    },
+
+    buttonStyle: {
+      backgroundColor: colorScheme.primary,
+      borderRadius: 1000,
+      padding: layout.padding / 2,
+      paddingHorizontal: layout.padding,
+    },
+
+    buttonTextStyle: {
+      ...typography.labelLarge,
+      color: colorScheme.onPrimary,
+    },
+
+    currentTextStyle: {
+      ...typography.labelLarge,
+      color: colorScheme.onSurface,
+    },
+  });
+};
+
+const localStyleCache = new Map<any, any>();
+const useLocalStyle = createCachedFactory(localStyleCache, createStyleSheet);
