676167d8b01447f23681640bf6c7fc3f32fec202
derRaab <github.com@derRaab.com>
2024-07-09 15:37:17 +0200

    Update app.json handling

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,8 @@
+import * as dotenv from "dotenv";
 import { ConfigContext, ExpoConfig } from "expo/config";
+
+// Load .env.local values if available
+dotenv.config({ path: ".env.local" });
 
 export default ({ config }: ConfigContext): ExpoConfig => {
   const newConfig: ExpoConfig = {
@@ -8,9 +12,27 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     slug: config.slug ?? "airflow-calculator",
   };
 
-  newConfig.ios = newConfig.ios || {};
-  newConfig.ios.bundleIdentifier =
-    process.env.EXPO_PUBLIC_IOS_BUNDLE_IDENTIFIER;
+  // Read values from the environment
+  const androidPackage = process.env.EXPO_SECRET_ANDROID_PACKAGE;
+  const extraEasProjectId = process.env.EXPO_SECRET_EXTRA_EAS_PROJECT_ID;
+  const iosBundleIdentifier = process.env.EXPO_SECRET_IOS_BUNDLE_IDENTIFIER;
+  const owner = process.env.EXPO_SECRET_OWNER;
+  const updatesUrl = process.env.EXPO_SECRET_UPDATES_URL;
+
+  newConfig.android = newConfig.android ?? {};
+  newConfig.android.package = androidPackage;
+
+  newConfig.extra = newConfig.extra ?? {};
+  newConfig.extra.eas = newConfig.extra.eas ?? {};
+  newConfig.extra.eas.projectId = extraEasProjectId;
+
+  newConfig.ios = newConfig.ios ?? {};
+  newConfig.ios.bundleIdentifier = iosBundleIdentifier;
+
+  newConfig.owner = owner;
+
+  newConfig.updates = newConfig.updates ?? {};
+  newConfig.updates.url = updatesUrl;
 
   return newConfig;
 };
