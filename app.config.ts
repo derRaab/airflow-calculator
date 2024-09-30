c404e3008a687e86c88292c17045bb4078ca87b2
derRaab <github.com@derRaab.com>
2024-09-30 12:42:16 +0200

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,17 @@
 import * as dotenv from "dotenv";
 import { ConfigContext, ExpoConfig } from "expo/config";
 
-// Load .env.local values if available
-dotenv.config({ path: ".env.local" });
+// We wrap the key name with underscores (e.g. __KEY__) as a placeholder for the actual value in eas.json and replace it with the real value from GitHub Action secrets.
+// To test and use the same script locally we fallback to values from a .env.local file.
+const dotenvLocal = dotenv.config({ path: ".env.local" });
+const getEnvValue = (key: string): string => {
+  const value: string = process.env[key] ?? "";
+  if (value === "" || "__" + key + "__" === value) {
+    const dotenvLocalValue = dotenvLocal.parsed?.[key] ?? "";
+    return dotenvLocalValue;
+  }
+  return value;
+};
 
 export default ({ config }: ConfigContext): ExpoConfig => {
   const newConfig: ExpoConfig = {
@@ -13,12 +22,12 @@ export default ({ config }: ConfigContext): ExpoConfig => {
   };
 
   // Read values from the environment
-  const appName = process.env.EXPO_SECRET_APP_NAME;
-  const androidPackage = process.env.EXPO_SECRET_ANDROID_PACKAGE;
-  const extraEasProjectId = process.env.EXPO_SECRET_EXTRA_EAS_PROJECT_ID;
-  const iosBundleIdentifier = process.env.EXPO_SECRET_IOS_BUNDLE_IDENTIFIER;
-  const owner = process.env.EXPO_SECRET_OWNER;
-  const updatesUrl = process.env.EXPO_SECRET_UPDATES_URL;
+  let appName = getEnvValue("EXPO_SECRET_APP_NAME");
+  let androidPackage = getEnvValue("EXPO_SECRET_ANDROID_PACKAGE");
+  let extraEasProjectId = getEnvValue("EXPO_SECRET_EXTRA_EAS_PROJECT_ID");
+  let iosBundleIdentifier = getEnvValue("EXPO_SECRET_IOS_BUNDLE_IDENTIFIER");
+  let owner = getEnvValue("EXPO_SECRET_OWNER");
+  let updatesUrl = getEnvValue("EXPO_SECRET_UPDATES_URL");
 
   newConfig.name = appName ?? newConfig.name;
 
